56763399bb840e5d3cbe27ad71f067c106933ccd
Daniel Braga <dbragabarbosa@gmail.com>
2024-02-01 00:25:48 -0300

    day2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,5 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let celsius : Double = 30.0
+let fahrenheit : Double = (((celsius * 9) / 5) + 32)
+print("Celsius: \(celsius)°  Fahrenheit: \(fahrenheit)°")
