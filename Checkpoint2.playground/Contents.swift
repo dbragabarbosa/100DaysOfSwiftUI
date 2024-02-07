f279ac4652c25ad6be77c160bf3407b624ef7b15
Daniel Braga <dbragabarbosa@gmail.com>
2024-02-07 17:58:20 -0300

    day5

--- Checkpoint2.playground/Contents.swift
+++ Checkpoint2.playground/Contents.swift
@@ -14,3 +14,23 @@ var tamanhoSet: Int = nomesUnicos.count
 
 print("Numero de itens: \(tamanhoArray)")
 print("Numero de itens unicos: \(tamanhoSet)")
+
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+let transporte : TransportOption = .car
+
+print(transporte)
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
