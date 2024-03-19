00747263c8456944d87bf8c13f660bbeb047e9b7
Daniel Braga <dbragabarbosa@gmail.com>
2024-03-19 19:21:25 -0300

    day14

--- day14.swift
+++ day14.swift
@@ -342,3 +342,29 @@ sending analytics to a server, for example.
 
 
 
+Summary: Optionals
+
+In these chapters we’ve covered one of Swift’s most important features, and although most people find optionals hard to understand at first almost everyone 
+agrees they are useful in practice.
+
+Let’s recap what we learned:
+
+Optionals let us represent the absence of data, which means we’re able to say “this integer has no value” – that’s different from a fixed number such as 0.
+
+As a result, everything that isn’t optional definitely has a value inside, even if that’s just an empty string.
+
+Unwrapping an optional is the process of looking inside a box to see what it contains: if there’s a value inside it’s sent back for use, otherwise there 
+will be nil inside.
+
+We can use if let to run some code if the optional has a value, or guard let to run some code if the optional doesn’t have a value – but with guard we 
+must always exit the function afterwards.
+
+The nil coalescing operator, ??, unwraps and returns an optional’s value, or uses a default value instead.
+
+Optional chaining lets us read an optional inside another optional with a convenient syntax.
+
+If a function might throw errors, you can convert it into an optional using try? – you’ll either get back the function’s return value, or nil if an error 
+is thrown.
+
+Optionals are second only to closures when it comes to language features folks struggle to learn, but I promise after a few months you’ll wonder how you 
+could live without them!
