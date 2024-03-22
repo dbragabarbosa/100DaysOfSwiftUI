e055b7401639d83ccbadf6a24f65736f50530745
Daniel Braga <dbragabarbosa@gmail.com>
2024-03-21 21:48:46 -0300

    day14

--- Checkpoint9.playground/Contents.swift
+++ Checkpoint9.playground/Contents.swift
@@ -1,34 +1,3 @@
 import Cocoa
 
-protocol Building
-{
-    var rooms: Int { get set }
-    var cost: Int { get set }
-    var estateAgent: String { get set }
-    
-    func printSalesSummary()
-}
 
-struct House: Building
-{
-    var rooms: Int
-    var cost: Int
-    var estateAgent: String
-    
-    func printSalesSummary() 
-    {
-        print("Sales summary!")
-    }
-}
-
-struct Office: Building
-{
-    var rooms: Int
-    var cost: Int
-    var estateAgent: String
-    
-    func printSalesSummary()
-    {
-        print("Sales summary!")
-    }
-}
