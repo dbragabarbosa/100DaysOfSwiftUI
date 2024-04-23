793f854e460e7cbe2e401c6494637bab8521e6d0
Daniel Braga <dbragabarbosa@gmail.com>
2024-04-22 22:28:14 -0300

    day14

--- day14.swift
+++ day14.swift
@@ -368,3 +368,14 @@ is thrown.
 
 Optionals are second only to closures when it comes to language features folks struggle to learn, but I promise after a few months you’ll wonder how you 
 could live without them!
+
+Checkpoint 9
+
+Now that you understand a little about optionals, it’s time to pause for a few minutes and try a small coding challenge so you can see how much you’ve 
+remembered.
+
+Your challenge is this: write a function that accepts an optional array of integers, and returns one randomly. If the array is missing or empty, return a 
+random number in the range 1 through 100.
+
+If that sounds easy, it’s because I haven’t explained the catch yet: I want you to write your function in a single line of code. No, that doesn’t mean you 
+should just write lots of code then remove all the line breaks – you should be able to write this whole thing in one line of code.
