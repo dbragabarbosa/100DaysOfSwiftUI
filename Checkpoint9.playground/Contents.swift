544eec826a0171bd983789014205a001529c45d2
Daniel Braga <dbragabarbosa@gmail.com>
2024-06-10 21:37:29 -0300

    day14

--- Checkpoint9.playground/Contents.swift
+++ Checkpoint9.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Cocoa
 
 
+func acceptOptArrayInts(arrayOfInts: [Int]?) -> Int
+{
+    if let arrayOfInts = arrayOfInts
+    {
+        return arrayOfInts.randomElement() ?? Int.random(in: 1...100)
+    }
+    else
+    {
+        return Int.random(in: 1...100)
+    }
+}
+
+
+func acceptOptArrayIntsInOneLine(arrayOfInts: [Int]?) -> Int
+{
+    return (arrayOfInts?.randomElement() ?? Int.random(in: 1...100))
+}
