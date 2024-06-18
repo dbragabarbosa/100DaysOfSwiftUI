1e436d8fadf424e22a8b9c13cc90a23f74e6f8d9
Daniel Braga <dbragabarbosa@gmail.com>
2024-06-18 13:28:22 -0300

    day16

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daniel Braga Barbosa on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview 
+{
+    ContentView()
+}
