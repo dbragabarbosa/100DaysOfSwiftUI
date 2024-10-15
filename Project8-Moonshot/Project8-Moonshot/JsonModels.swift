e50c81f5ff9459627c4766e1b76c1aa14ea34f56
Daniel Braga <dbragabarbosa@gmail.com>
2024-10-15 10:14:16 -0300

    day41

---
+++ Project8-Moonshot/Project8-Moonshot/JsonModels.swift
@@ -0,0 +1,14 @@
+//
+//  JsonModels.swift
+//  Project8-Moonshot
+//
+//  Created by Daniel Braga Barbosa on 14/10/24.
+//
+
+import Foundation
+
+enum JsonModels: String
+{
+    case astronauts = "astronauts.json"
+    case missions = "missions.json"
+}
