85cd5d4448e8b5209029437328d8f51a0ac54a49
Daniel Braga <dbragabarbosa@gmail.com>
2024-11-03 22:44:21 -0300

    day57

---
+++ Project12-SwiftDataProject/Project12-SwiftDataProject/Project12_SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project12_SwiftDataProjectApp.swift
+//  Project12-SwiftDataProject
+//
+//  Created by Daniel Braga Barbosa on 03/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project12_SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
