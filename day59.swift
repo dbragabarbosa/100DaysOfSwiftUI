bc52109836ab8a78ce421d845e2488d4565c85b1
Daniel Braga <dbragabarbosa@gmail.com>
2024-11-06 21:58:32 -0300

    day59

---
+++ day59.swift
@@ -0,0 +1,24 @@
+Project 12, part 3
+Are you ready for some more challenges?
+
+American singer and songwriter Christina Grimmie once said, “people aren't born strong; People grow stronger little by little, encountering difficult situations, learning not to run from them.” The same is absolutely true of good coders: there’s no magic switch that gets flicked when you’ve been reading books for a year, but instead your skills grow slowly over time as you face – and solve – increasingly hard problems.
+
+I keep encouraging you to tackle these problems because writing your own code, and finding your own solutions, matters. At first there will often be some blank page syndrome – where you stare blankly at the screen, not knowing where to start. But that’s OK, and in fact it’s to be expected. The more you practice the more adept you’ll become at figuring out solutions for yourself, and today is another step towards that goal.
+
+Today you should work through the wrap up chapter for project 12, complete its review, then work through all three of its challenges.
+
+SwiftData: Wrap up
+
+Although we've only really scratched the surface of what SwiftData can do, you’ve still seen how it can add, delete, sort, filter, and more, all with relatively simple code. Yes, a few parts are a little murky in Swift – #Predicate, for example, can take some getting used to – but as long as you're careful you should be fine.
+
+Perhaps the most important thing about SwiftData is that it’s guaranteed to be there for all apps, on all of Apple’s platforms. This means you can use it regardless of your needs: maybe it’s for saving important data, maybe it’s just a cache of content you downloaded; it doesn’t matter, because SwiftData will do a great job of managing it for you.
+
+
+Challenge
+One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
+
+All three of these challenges relate to you upgrade project 7, iExpense:
+
+Start by upgrading it to use SwiftData.
+Add a customizable sort order option: by name or by amount.
+Add a filter option to show all expenses, just personal expenses, or just business expenses.
