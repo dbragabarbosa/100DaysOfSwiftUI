7094cb8c1c083424455ff9628b8b495070f1aa74
Daniel Braga <dbragabarbosa@gmail.com>
2024-11-13 18:10:13 -0300

    day61

---
+++ Project13-Instafilter/Project13-Instafilter/Project13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project13_InstafilterApp.swift
+//  Project13-Instafilter
+//
+//  Created by Daniel Braga Barbosa on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
