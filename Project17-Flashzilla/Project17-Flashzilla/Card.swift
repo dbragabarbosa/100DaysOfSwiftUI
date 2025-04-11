0b2f19c6464f91cd98c3ddbf994d1851b749f499
Daniel Braga <dbragabarbosa@gmail.com>
2025-04-11 19:29:21 -0300

    day88

---
+++ Project17-Flashzilla/Project17-Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Project17-Flashzilla
+//
+//  Created by Daniel Braga Barbosa on 11/04/25.
+//
+
+import Foundation
+
+struct Card
+{
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What is Swift?", answer: "A high-level programming language.")
+}
+
--- Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
@@ -9,17 +9,50 @@ import SwiftUI
 
 struct ContentView: View
 {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View
     {
-        VStack
+        ZStack
         {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
-            Text("Hello, world!")
+            VStack
+            {
+                ZStack
+                {
+                    ForEach(0..<cards.count, id: \.self)
+                    { index in
+                        
+                        CardView(card: cards[index])
+                        {
+                            withAnimation
+                            {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeCard(at index: Int)
+    {
+        cards.remove(at: index)
+    }
+    
+}
+
+extension View
+{
+    func stacked(at position: Int, in total: Int) -> some View
+    {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
     }
 }
 
