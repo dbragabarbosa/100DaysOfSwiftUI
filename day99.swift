2fd49019b57bec6d935824319101d1bd5529eb43
Daniel Braga <dbragabarbosa@gmail.com>
2025-05-28 08:17:39 -0300

    day99

---
+++ day99.swift
@@ -0,0 +1,22 @@
+Project 19, part 4
+
+Now that our app is finished it’s time to take on some challenges to help solidify your knowledge, then take a quick test to make sure you remember what you learned.
+
+Although the first two challenges are fairly easy this time, I think you’ll find the third one more taxing. Hopefully you’ve built up some firm resilience over the last 99 days of work, but even then I hope you remember it’s OK to fail – as the Spanish writer Jose Bergamin said, “to be ready to fail is to be prepared for success.”
+
+Today you should work through the wrap up chapter for project 19, complete its review, then work through all three of its challenges.
+
+
+SnowSeeker: Wrap up
+
+This wasn’t a particularly complicated project, but it still taught new skills such as split view layouts, optional alerts, layout with transparent groups, custom environment classes, and even an improved way of formatting lists in text. It also gave you the chance to practice lots of core skills, such as handling Codable, creating scrolling lists, and more.
+
+This kind of app is really good as a template, because it’s the kind of thing you can use again and again in the future just by varying the kind of content you feed into it. Template apps – as well as the techniques underlying them – are the “bread and butter” of iOS apps: the kind of thing that is never going to set the world on fire, but also so fundamentally useful and common that you can’t do without them.
+
+
+Challenge
+One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on.
+
+Add a photo credit over the ResortView image. The data is already loaded from the JSON for this purpose, so you just need to make it look good in the UI.
+Fill in the loading and saving methods for Favorites.
+For a real challenge, let the user sort the resorts in ContentView either using the default order, alphabetical order, or country order.
