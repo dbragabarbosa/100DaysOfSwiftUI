7aaeddeb1861d0c4c018a0a7c6345335e1cd8c8b
Daniel Braga <dbragabarbosa@gmail.com>
2025-05-28 08:17:54 -0300

    day100

---
+++ day100.swift
@@ -0,0 +1,25 @@
+Final exam
+Today is the final day of the 100 Days of SwiftUI, which means you made it. You went through all 100 days of learning, watching videos, taking tests, completing challenges, and writing code, and you made it to the end.
+
+Even though we’ve probably never met, I feel proud of you. Nothing about this challenge was easy: I made you study every day, I pushed you to answer well over a thousand questions about Swift and iOS, and I gave you difficult challenges to complete.
+
+I have no doubt that some of the work was difficult for you – you might have sometimes repeated days, you might have skipped a few challenges here and there, and it’s possible you even thought about giving up more than once.
+
+But you didn’t give up – you made it. As Aisha Tyler said, “success is not the absence of failure; it's the persistence through failure.” And that’s what you’ve shown: even when things weren’t going to plan with your code you stuck with it – you persevered – and you got through all 19 projects and more as a result.
+
+Today you have one last test: the final exam for the 100 Days of SwiftUI. This one is different from the others in four important ways:
+
+You’ll be tested across all parts of the curriculum, from the first project to the last.
+The questions almost entirely one ones you’ve seen before, although perhaps not for a few months in some cases.
+You get a certificate when you pass the test. You should download this, and share it with others – you earned it!
+Now, I said there were four differences and only listed three above. That’s because the fourth difference might scare you a little, so here goes: there are 100 questions.
+
+Yes, 100.
+
+There’s no reading to do today; no videos to watch. This exam is the only thing you need to complete. This exam is the only thing between you and graduating from this challenge.
+
+So, set aside an hour of your time, get a drink next to your computer, and put yourself in the right in the right state of mind. Most importantly: take your time. There’s no point rushing through this.
+
+Out of the 100 questions, you’ll need to get at least 70 correct in order to pass the test. If you score at least 80 percent you’ll pass with merit, and you score at least 95 percent – getting just five questions wrong out of 100 – you’ll pass with distinction.
+
+Good luck.
